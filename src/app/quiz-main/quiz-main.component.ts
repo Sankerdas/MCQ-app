74af6fb8f45b90c22a70b4fdceb3b158691f519c
Sanker das <sankerdas.yes@gmail.com>
2019-07-21 22:07:34 +0530

    lap: quiz listed and basic quiz result section created
    quiz qns and ans listed, correct wrong answers calculated and basic result loged

--- src/app/quiz-main/quiz-main.component.ts
+++ src/app/quiz-main/quiz-main.component.ts
@@ -12,13 +12,38 @@ export class QuizMainComponent implements OnInit {
   constructor(public qnsService: QnsService) { }
 
   qzQns: any;
+  C = 0;
+  W = 0;
+
 
   getQuizQns = () => {
     this.qnsService.getQns().subscribe(res => (this.qzQns = res));
   }
 
+
+  qzCkeck = (data, opn, id) => {
+
+    if (data.ans === opn ) {
+      this.C++;
+      console.log('Correct ' + this.C);
+
+
+    } else {
+      this.W++;
+      console.log('Wrong ' + this.W);
+    }
+
+  }
+
+  qzResult = () => {
+    console.log('Wrong ' + this.W);
+    console.log('Correct ' + this.C);
+  }
+
+
   ngOnInit() {
     this.getQuizQns();
   }
+
 
 }
