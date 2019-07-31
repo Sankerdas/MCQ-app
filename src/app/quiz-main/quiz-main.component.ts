e5bcf4c7818050110f3eb51bcd5fc61c341fc4d7
Sanker das <sankerdas.yes@gmail.com>
2019-07-31 18:24:51 +0530

    PC: timer added
    timer will run at loading and stops when clicked on result button

--- src/app/quiz-main/quiz-main.component.ts
+++ src/app/quiz-main/quiz-main.component.ts
@@ -10,39 +10,51 @@ import { QnsService } from '../shared/qns.service';
 export class QuizMainComponent implements OnInit {
 
   constructor(public qnsService: QnsService) { }
-
   qzQns: any;
   C = 0;
   W = 0;
   rslt: any;
+  second = 0; minute = 0;
+  tRun = true;
 
   getQuizQns = () => {
     this.qnsService.getQns().subscribe(res => (this.qzQns = res));
   }
 
-
   qzCkeck = (data, opn) => {
-
     if (data.ans === opn ) {
       this.C++;
       console.log('Correct ' + this.C);
-
-
     } else {
       this.W++;
       console.log('Wrong ' + this.W);
     }
-
   }
 
   qzResult = () => {
     this.rslt = ' Correct Answer(s) ' + this.C + ' Wrong Answer(s) ' + this.W;
+    this.tRun = false;
     console.log(this.rslt);
   }
 
+  add = () => {
+    if (this.tRun) {
+    this.second++;
+    if (this.second > 60 ) {
+      this.minute++;
+      this.second = 0;
+      }
+    this.callTime();
+  }
+  }
+
+  callTime = () => {
+    setTimeout(() => { this.add(); }, 1000);
+  }
 
   ngOnInit() {
     this.getQuizQns();
+    this.callTime();
   }
 
 
